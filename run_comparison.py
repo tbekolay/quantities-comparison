f1c71291e3c590b5dbd76c4bea536ada92cef02a
Todd Jennings <toddrjen@gmail.com>
2014-04-01 19:16:39 +0200

    allow exporting data as pandas DataFrame objects

--- run_comparison.py
+++ run_comparison.py
@@ -16,6 +16,7 @@ import json
 import warnings
 
 import numpy as np
+import pandas as pd
 
 warnings.simplefilter('ignore')
 np.seterr(all='ignore')
@@ -34,11 +35,10 @@ CLASSES = (bm.bench_astropy.BenchAstropy,
            )
 
 
-def get_comparisons(classes=CLASSES):
-    res = []
+def run_comparisons(classes=CLASSES):
     for cls in classes:
         print cls.__name__
-        res.append(bm.base.bench(cls))
+        yield bm.base.bench(cls)
 
 
 def save_comparisons(res, fname=None):
@@ -46,13 +46,66 @@ def save_comparisons(res, fname=None):
         fname = 'results.json'
 
     with open(fname, 'w') as outfile:
-        json.dump(res, outfile, indent=2, separators=(',', ': '))
+        json.dump(list(res), outfile, indent=2, separators=(',', ': '))
 
 
-def run(classes=CLASSES, fname=None):
-    res = get_comparisons(classes)
+def get_comparisons(classes=CLASSES, fname=None):
+    res = run_comparisons(classes)
     save_comparisons(res, fname)
 
 
+def process_pandas(res):
+    facts = {}
+    syntax = {}
+    speed = {}
+    compatibility = {}
+
+    for ires in res:
+        name = ires['name']
+        facts[name] = ires['facts']
+        syntax[name] = ires['syntax']
+
+        # We want to transpose the speed dict so it is organized
+        # by operation type rather than measurement.
+        # With pandas 1.4 we should be ablle to add error bars as well.
+        for key, value in ires['speed'].items():
+            for key1, value1 in value.items():
+                if key1 not in speed:
+                    speed[key1] = {}
+                if key not in speed[key1]:
+                    speed[key1][key] = {}
+                speed[key1][key][name] = value1
+
+        for key, value in ires['compatibility'].items():
+            if key not in compatibility:
+                compatibility[key] = {}
+            compatibility[key][name] = value
+
+    facts = pd.DataFrame.from_dict(facts, orient='index')
+    syntax = pd.DataFrame.from_dict(syntax, orient='index')
+
+    facts = facts.sort(axis=0).sort(axis=1)
+    syntax = syntax.sort(axis=0).sort(axis=1)
+
+    for key, value in speed.items():
+        value = pd.DataFrame.from_dict(value, orient='columns')
+        speed[key] = value.sort(axis=0).sort(axis=1)
+    for key, value in compatibility.items():
+        value = pd.DataFrame.from_dict(value, orient='index')
+        compatibility[key] = value.sort(axis=0).sort(axis=1)
+
+    resdict = {'facts': facts,
+               'syntax': syntax,
+               'speed': speed,
+               'compatibility': compatibility}
+
+    return resdict
+
+
+def get_pandas(classes=CLASSES):
+    res = run_comparisons(classes)
+    return process_pandas(res)
+
+
 if __name__ == "__main__":
-    run()
+    get_comparisons()
